6e8089762f472575d45f2e3d548274d7b0324cc4
Jonas Schneider-Bensch <jonas@cryspen.com>
2023-07-12 13:40:10 +0200

    Tests for expand_message_xmd

--- hacspec-scrambledb/hash-to-curve/src/p256_hash.rs
+++ hacspec-scrambledb/hash-to-curve/src/p256_hash.rs
@@ -110,3 +110,112 @@ pub fn hash_to_curve(msg: &[u8], dst: &[u8]) -> p256::G {
     let p = r.clear_cofactor();
     p
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use lazy_static::lazy_static;
+    use serde_json::Value;
+
+    fn load_vectors(path: &str) -> Value {
+        use std::fs;
+        serde_json::from_str(&fs::read_to_string(path).expect("File not found.")).unwrap()
+    }
+
+    lazy_static! {
+        static ref VECTORS_EXPAND_MESSAGE_XMD_SHA256_38: serde_json::Value =
+            load_vectors("expand_message_xmd_SHA256_38.json");
+        static ref VECTORS_P256_XMD_SHA256_SSWU_RO: serde_json::Value =
+            load_vectors("P256_XMD_SHA-256_SSWU_RO_P256_XMD_SHA-256_SSWU_RO_.json");
+    }
+
+    #[test]
+    fn test_dst_prime() {
+        let dst = VECTORS_EXPAND_MESSAGE_XMD_SHA256_38["DST"]
+            .as_str()
+            .unwrap();
+        let dst = dst.as_bytes();
+
+        let mut test_cases = VECTORS_EXPAND_MESSAGE_XMD_SHA256_38["tests"]
+            .as_array()
+            .unwrap()
+            .clone();
+        let test_case = test_cases.pop().unwrap();
+
+        let dst_prime_expected = test_case["DST_prime"].as_str().unwrap();
+        let dst_prime_expected = hex::decode(dst_prime_expected).unwrap();
+        assert_eq!(dst_prime_expected, dst_prime(&dst));
+    }
+
+    #[test]
+    fn test_msg_prime() {
+        let test_cases = VECTORS_EXPAND_MESSAGE_XMD_SHA256_38["tests"]
+            .as_array()
+            .unwrap()
+            .clone();
+        for test_case in test_cases.iter() {
+            let msg = test_case["msg"].as_str().unwrap();
+            let msg = msg.as_bytes();
+
+            let msg_prime_expected = test_case["msg_prime"].as_str().unwrap();
+            let msg_prime_expected = hex::decode(msg_prime_expected).unwrap();
+
+            let dst_prime = test_case["DST_prime"].as_str().unwrap();
+            let dst_prime = hex::decode(dst_prime).unwrap();
+
+            let len_in_bytes = test_case["len_in_bytes"]
+                .as_str()
+                .unwrap()
+                .trim_start_matches("0x");
+            let len_in_bytes = usize::from_str_radix(len_in_bytes, 16).unwrap();
+
+            assert_eq!(msg_prime_expected, msg_prime(msg, &dst_prime, len_in_bytes));
+        }
+    }
+
+    #[test]
+    fn test_expand_message_xmd() {
+        let dst = VECTORS_EXPAND_MESSAGE_XMD_SHA256_38["DST"]
+            .as_str()
+            .unwrap();
+        let dst = dst.as_bytes();
+
+        let test_cases = VECTORS_EXPAND_MESSAGE_XMD_SHA256_38["tests"]
+            .as_array()
+            .unwrap()
+            .clone();
+        for test_case in test_cases.iter() {
+            let msg = test_case["msg"].as_str().unwrap();
+            let msg = msg.as_bytes();
+
+            let len_in_bytes = test_case["len_in_bytes"]
+                .as_str()
+                .unwrap()
+                .trim_start_matches("0x");
+            let len_in_bytes = usize::from_str_radix(len_in_bytes, 16).unwrap();
+
+            let uniform_bytes_expected = test_case["uniform_bytes"].as_str().unwrap();
+            let uniform_bytes_expected = hex::decode(uniform_bytes_expected).unwrap();
+
+            assert_eq!(
+                uniform_bytes_expected,
+                expand_message_xmd(msg, &dst, len_in_bytes)
+            );
+        }
+    }
+
+    #[test]
+    fn test_hash_to_field() {
+        unimplemented!()
+    }
+
+    #[test]
+    fn test_map_to_curve() {
+        unimplemented!()
+    }
+
+    #[test]
+    fn test_hash_to_curve() {
+        unimplemented!()
+    }
+}
