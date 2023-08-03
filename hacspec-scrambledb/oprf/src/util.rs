3513dbaade30d5d76dbd35df67f45186d35a03da
Jonas Schneider-Bensch <jonas@cryspen.com>
2023-08-03 10:16:30 +0200

    Comment should point to draft section that is implemented

--- hacspec-scrambledb/oprf/src/util.rs
+++ hacspec-scrambledb/oprf/src/util.rs
@@ -1,12 +1,15 @@
 use p256::{NatMod, P256Scalar};
 use rand::prelude::*;
 
+/// ### 4.7.2 Random Number Generation Using Extra Random Bits
+///
 /// Generate a random byte array with L = ceil(((3 *
 /// ceil(log2(G.Order()))) / 2) / 8) bytes, and interpret it as an
 /// integer; reduce the integer modulo G.Order() and return the result.
 /// See [I-D.irtf-cfrg-hash-to-curve], Section 5 for the underlying
 /// derivation of L.
 ///
+/// For P-256:
 /// ceil(log2(G.Order())) = 256
 /// ceil(((3 * 256) / 2) / 8) = 48
 pub fn random_scalar() -> P256Scalar {
