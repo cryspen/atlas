e5f94a22a8e3dad53dfde450390e45d419482e73
Jonas Schneider-Bensch <jonas@cryspen.com>
2023-08-09 14:58:25 +0200

    Testing the rest of the protocol

--- hacspec-scrambledb/oprf/src/test_util.rs
+++ hacspec-scrambledb/oprf/src/test_util.rs
@@ -1,7 +1,10 @@
-use crate::protocol::configuration::create_context_string;
+use crate::protocol::online::oprf::{blind_evaluate, evaluate};
 use crate::protocol::setup::derive_key_pair;
+use crate::protocol::{configuration::create_context_string, online::oprf::finalize};
 use std::fs::read_to_string;
 
+use p256::{NatMod, P256Point};
+use serde::de::IntoDeserializer;
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -45,9 +48,61 @@ pub fn load_vectors(path: &str) -> Vec<TestVector> {
 }
 
 #[test]
-fn oprf() {
+fn oprf_blind_evaluate() {
     use p256::{NatMod, P256Scalar};
-    let _ = pretty_env_logger::try_init();
+
+    let tests = load_vectors("allVectors.json");
+
+    for test in tests {
+        if test.identifier == "P256-SHA256" && test.mode == 0 {
+            let skS = P256Scalar::from_be_bytes(&test.skSm);
+
+            for batch in test.vectors {
+                let blindedElements: Vec<p256::P256Point> = batch
+                    .BlindedElement
+                    .split(",")
+                    .map(|b| {
+                        crate::p256_sha256::deserialize_element(
+                            hex::decode(b).unwrap().try_into().unwrap_or_else(|_| {
+                                panic!(
+                                    "Attempt to deserialize invalid byte string: {:?} ({})",
+                                    b.as_bytes(),
+                                    b.as_bytes().len()
+                                )
+                            }),
+                        )
+                        .expect("Could not deserialize")
+                    })
+                    .collect();
+
+                let expected_evaluatedElements: Vec<p256::P256Point> = batch
+                    .EvaluationElement
+                    .split(",")
+                    .map(|e| {
+                        crate::p256_sha256::deserialize_element(
+                            hex::decode(e)
+                                .unwrap()
+                                .try_into()
+                                .expect("Attempt to deserialize invalid byte string."),
+                        )
+                        .expect("Could not deserialize")
+                    })
+                    .collect();
+
+                for (blindedElement, expected_evaluatedElement) in
+                    blindedElements.iter().zip(expected_evaluatedElements)
+                {
+                    let evaluatedElement = blind_evaluate(skS, *blindedElement).unwrap();
+                    assert_eq!(evaluatedElement, expected_evaluatedElement);
+                }
+            }
+        }
+    }
+}
+
+#[test]
+fn oprf_key_derivation() {
+    use p256::{NatMod, P256Scalar};
     let tests = load_vectors("allVectors.json");
 
     for test in tests {
@@ -57,17 +112,86 @@ fn oprf() {
             let (skS, _pkS) = derive_key_pair(&test.seed, &test.keyInfo, &context_string).unwrap();
 
             assert_eq!(skS, P256Scalar::from_be_bytes(&test.skSm));
+        }
+    }
+}
 
+#[test]
+fn oprf_finalize() {
+    use p256::{NatMod, P256Scalar};
+
+    let tests = load_vectors("allVectors.json");
+
+    for test in tests {
+        if test.identifier == "P256-SHA256" && test.mode == 0 {
             for batch in test.vectors {
-                let (blind, blindedElement) = crate::protocol::online::oprf::blind(batch.Input.as_bytes(), &context_string).unwrap();
+                let Inputs: Vec<Vec<u8>> = batch
+                    .Input
+                    .split(",")
+                    .map(|i| hex::decode(i).unwrap())
+                    .collect();
+                let Blinds: Vec<P256Scalar> = batch
+                    .Blind
+                    .split(",")
+                    .map(|b| P256Scalar::from_be_bytes(&hex::decode(b).unwrap()))
+                    .collect();
+                let evaluatedElements: Vec<p256::P256Point> = batch
+                    .EvaluationElement
+                    .split(",")
+                    .map(|e| {
+                        crate::p256_sha256::deserialize_element(
+                            hex::decode(e)
+                                .unwrap()
+                                .try_into()
+                                .expect("Attempt to deserialize invalid byte string."),
+                        )
+                        .expect("Could not deserialize")
+                    })
+                    .collect();
+                let expectedOutputs: Vec<Vec<u8>> = batch
+                    .Output
+                    .split(",")
+                    .map(|o| hex::decode(o).unwrap())
+                    .collect();
 
-                assert_eq!(blind, P256Scalar::from_be_bytes(batch.Blind.as_bytes()));
-                //assert_eq!(blindedElement, )
+                for i in 0..batch.Batch {
+                    let output = finalize(&Inputs[i], Blinds[i], evaluatedElements[i]).unwrap();
+                    assert_eq!(output, expectedOutputs[i]);
+                }
             }
         }
     }
 }
 
-// thread 'test_util::oprf' panicked at 'assertion failed: `(left == right)`
-//   left: `P256Scalar { value: [29, 19, 55, 92, 235, 247, 191, 145, 185, 100, 189, 206, 66, 50, 249, 0, 21, 31, 157, 81, 57, 17, 53, 212, 245, 21, 194, 198, 23, 246, 252, 50] }`,
-//  right: `P256Scalar { value: [243, 180, 110, 135, 165, 154, 17, 212, 239, 235, 2, 136, 147, 182, 119, 116, 89, 27, 185, 165, 161, 56, 18, 157, 52, 201, 249, 134, 29, 27, 134, 77] }`', oprf/src/test_util.rs:64:17
+#[test]
+fn oprf_evaluate() {
+    use p256::{NatMod, P256Scalar};
+
+    let tests = load_vectors("allVectors.json");
+
+    for test in tests {
+        if test.identifier == "P256-SHA256" && test.mode == 0 {
+            let skS = P256Scalar::from_be_bytes(&test.skSm);
+            let context_string =
+                create_context_string(test.mode.into(), test.identifier.as_bytes());
+
+            for batch in test.vectors {
+                let Inputs: Vec<Vec<u8>> = batch
+                    .Input
+                    .split(",")
+                    .map(|i| hex::decode(i).unwrap())
+                    .collect();
+                let expectedOutputs: Vec<Vec<u8>> = batch
+                    .Output
+                    .split(",")
+                    .map(|o| hex::decode(o).unwrap())
+                    .collect();
+
+                for case in 0..batch.Batch {
+                    let output = evaluate(skS, &Inputs[case], &context_string).unwrap();
+                    assert_eq!(output, expectedOutputs[case]);
+                }
+            }
+        }
+    }
+}
