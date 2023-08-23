80b35a200968507271643c6fac1d91f786819be0
Jonas Schneider-Bensch <jonas@cryspen.com>
2023-08-23 11:12:36 +0200

    Doc- instead of code comments

--- hacspec-scrambledb/scrambledb/src/lib.rs
+++ hacspec-scrambledb/scrambledb/src/lib.rs
@@ -4,9 +4,10 @@ pub enum Error {
     PseudonomizationError,
     JoinError,
 }
-
-const SECPAR_BYTES: usize = 16; // security parameter in bytes;
-const RANDBYTES_SCALAR: usize = 32; // bytes of randomness required to sample scalars
+/// security parameter in bytes
+const SECPAR_BYTES: usize = 16;
+/// bytes of randomness required to sample scalars
+const RANDBYTES_SCALAR: usize = 32;
 
 pub mod converter;
 pub mod lake;
