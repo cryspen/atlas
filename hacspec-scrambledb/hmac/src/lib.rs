e8deae6c9c9a8789ba4703730e966fc01d205ec5
Jonas Schneider-Bensch <jonas@cryspen.com>
2023-08-25 16:08:09 +0200

    HKDF bugfix

--- hacspec-scrambledb/hmac/src/lib.rs
+++ hacspec-scrambledb/hmac/src/lib.rs
@@ -70,14 +70,14 @@ pub fn hkdf_extract<H: Hash>(salt: &[u8], ikm: &[u8]) -> Vec<u8> {
 pub fn hkdf_expand<H: Hash>(prk: &[u8], info: &[u8], l: usize) -> Vec<u8> {
     let n = (l + H::HASH_LEN - 1) / H::HASH_LEN; // N = ceil(L/HashLen)
 
-    let t = hmac::<H>(prk, &info.concat_byte(1u8));
+    let mut t = hmac(prk, &info.concat_byte(1u8));
     for i in 1..n {
         let round_input = t[i * H::HASH_LEN..(i + 1) * H::HASH_LEN]
             .to_vec()
             .concat(info)
             .concat_byte((i - 1) as u8);
-        let t_i = hmac::<H>(prk, &round_input);
-        t.concat(&t_i);
+        let t_i = hmac(prk, &round_input);
+        t.extend_from_slice(&t_i);
     }
     t[0..l].to_vec()
 }
