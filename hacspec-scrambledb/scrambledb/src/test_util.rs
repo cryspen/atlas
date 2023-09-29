cab0a09a5e613f915078c76fcb10a4fe3db378f3
Jonas Schneider-Bensch <jonas@cryspen.com>
2023-09-29 08:42:09 +0200

    Remove commented out old code for test table generation

--- hacspec-scrambledb/scrambledb/src/test_util.rs
+++ hacspec-scrambledb/scrambledb/src/test_util.rs
@@ -5,48 +5,24 @@ pub fn generate_plain_table() -> PlainTable {
     columns.push(Column::new(
         String::from("Address"),
         vec![
-            (
-                String::from("Alice"),
-                b"TestData1".to_vec(),
-                //hash_to_curve::p256_hash::hash_to_curve(b"TestData1", b"sample_dst").unwrap(),
-            ),
-            (
-                String::from("Bob"),
-                b"TestData2".to_vec(),
-                //hash_to_curve::p256_hash::hash_to_curve(b"TestData2", b"sample_dst").unwrap(),
-            ),
+            (String::from("Alice"), b"TestData1".to_vec()),
+            (String::from("Bob"), b"TestData2".to_vec()),
         ],
     ));
 
     columns.push(Column::new(
         String::from("Date of Birth"),
         vec![
-            (
-                String::from("Alice"),
-                b"TestData3".to_vec(),
-                //hash_to_curve::p256_hash::hash_to_curve(b"TestData3", b"sample_dst").unwrap(),
-            ),
-            (
-                String::from("Bob"),
-                b"TestData4".to_vec(),
-                //hash_to_curve::p256_hash::hash_to_curve(b"TestData4", b"sample_dst").unwrap(),
-            ),
+            (String::from("Alice"), b"TestData3".to_vec()),
+            (String::from("Bob"), b"TestData4".to_vec()),
         ],
     ));
 
     columns.push(Column::new(
         String::from("Favorite Color"),
         vec![
-            (
-                String::from("Alice"),
-                b"TestData5".to_vec(),
-                //hash_to_curve::p256_hash::hash_to_curve(b"TestData5", b"sample_dst").unwrap(),
-            ),
-            (
-                String::from("Bob"),
-                b"TestData6".to_vec(),
-                //hash_to_curve::p256_hash::hash_to_curve(b"TestData6", b"sample_dst").unwrap(),
-            ),
+            (String::from("Alice"), b"TestData5".to_vec()),
+            (String::from("Bob"), b"TestData6".to_vec()),
         ],
     ));
 
