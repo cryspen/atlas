e57e96c0e3958c5a77a61a2295eefacb4f94f944
Jonas Schneider-Bensch <jonas@cryspen.com>
2024-02-28 14:42:12 +0100

    Introduce an overarching `Error` type

--- mpc-spec/src/lib.rs
+++ mpc-spec/src/lib.rs
@@ -3,6 +3,19 @@
 //! This crate is an executable specification of an MPC engine based on the
 //! WRK17 protocol.
 
+use circuit::CircuitError;
+
+#[derive(Debug)]
+/// An error type.
+pub enum Error {
+    /// More random bytes have been asked for than are available.
+    InsufficientRandomness,
+    /// An error during circuit processing
+    Circuit(CircuitError),
+    /// Miscellaneous error.
+    OtherError,
+}
+
 /// The computational security parameter, in bytes.
 pub const COMPUTATIONAL_SECURITY: usize = 128 / 8;
 
