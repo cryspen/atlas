7593d06bd990ffa277f9bf9ad532f14148000c8a
Jonas Schneider-Bensch <jonas@cryspen.com>
2024-02-28 14:41:36 +0100

    Introduce computational and statistical security parameter constants

--- mpc-spec/src/lib.rs
+++ mpc-spec/src/lib.rs
@@ -3,4 +3,10 @@
 //! This crate is an executable specification of an MPC engine based on the
 //! WRK17 protocol.
 
+/// The computational security parameter, in bytes.
+pub const COMPUTATIONAL_SECURITY: usize = 128 / 8;
+
+/// The statistical security parameter, in bytes.
+pub const STATISTICAL_SECURITY: usize = 128 / 8;
+
 pub mod circuit;
