9b6d3db999b75c736d464827fd0a1e22d3198934
Jonas Schneider-Bensch <jonas@cryspen.com>
2024-04-18 09:22:44 +0200

    Update example: Now without ideal functionality

---
+++ atlas-spec/mpc-engine/examples/run_mpc.rs
@@ -0,0 +1,54 @@
+use std::thread;
+
+use hacspec_lib::Randomness;
+use mpc_engine::circuit::{Circuit, WiredGate};
+
+use rand::RngCore;
+
+fn build_circuit() -> Circuit {
+    Circuit {
+        input_widths: vec![1, 1, 1, 1],
+        gates: vec![
+            WiredGate::Input(0),  // Gate 0
+            WiredGate::Input(1),  // Gate 1
+            WiredGate::Input(2),  // Gate 2
+            WiredGate::And(0, 1), // Gate 3
+            WiredGate::And(3, 2), // Gate 4
+        ],
+        output_gates: vec![4],
+    }
+}
+fn main() {
+    let circuit = build_circuit();
+
+    let num_parties = circuit.number_of_parties();
+
+    // Set up channels
+    let mut party_channels = mpc_engine::utils::set_up_channels(num_parties);
+
+    let mut party_join_handles = Vec::new();
+    for _i in 0..num_parties {
+        let channel_config = party_channels
+            .pop()
+            .expect("every party should have a channel configuration");
+        let c = circuit.clone();
+        let party_join_handle = thread::spawn(move || {
+            let mut rng = rand::thread_rng();
+            let mut bytes = [0u8; 500];
+            rng.fill_bytes(&mut bytes);
+            let rng = Randomness::new(bytes.to_vec());
+            let mut p = mpc_engine::party::Party::new(channel_config, &c, rng);
+
+            let _ = p.run();
+        });
+        party_join_handles.push(party_join_handle);
+    }
+
+    for _i in 0..num_parties {
+        party_join_handles
+            .pop()
+            .expect("every party should have a join handle")
+            .join()
+            .expect("party did not panic");
+    }
+}
