4101d462b1357858cf70dde5fafdbbab4dd6cac6
Jonas Schneider-Bensch <jonas@cryspen.com>
2024-05-16 14:58:03 +0200

    Simple partial (de)serialization for authenticated bits

--- atlas-spec/mpc-engine/src/primitives/auth_share.rs
+++ atlas-spec/mpc-engine/src/primitives/auth_share.rs
@@ -1,4 +1,6 @@
 //! This module defines the interface for share authentication.
+use crate::{primitives::mac::MAC_LENGTH, Error};
+
 use super::mac::{Mac, MacKey};
 
 /// A bit held by a party with a given ID.
@@ -20,6 +22,42 @@ pub struct AuthBit {
     pub(crate) bit: Bit,
     pub(crate) macs: Vec<(usize, Mac)>,
     pub(crate) mac_keys: Vec<BitKey>,
+}
+
+impl AuthBit {
+    /// Serialize the bit value and all MACs on the bit.
+    pub fn serialize_bit_macs(&self) -> Vec<u8> {
+        let mut result = vec![0u8; (self.macs.len() + 1) * MAC_LENGTH + 1];
+        result[0] = self.bit.value as u8;
+        for (key_holder, mac) in self.macs.iter() {
+            result[1 + key_holder * MAC_LENGTH..1 + (key_holder + 1) * MAC_LENGTH]
+                .copy_from_slice(mac);
+        }
+
+        result
+    }
+
+    /// Deserialize a bit and MACs on that bit.
+    pub fn deserialize_bit_macs(bytes: &[u8]) -> Result<(bool, Vec<[u8; MAC_LENGTH]>), Error> {
+        if bytes[0] > 1 {
+            return Err(Error::InvalidSerialization);
+        }
+        let bit_value = bytes[0] != 0;
+        let mac_chunks = bytes[1..].chunks_exact(MAC_LENGTH);
+        if !mac_chunks.remainder().is_empty() {
+            return Err(Error::InvalidSerialization);
+        }
+
+        let mut macs: Vec<[u8; MAC_LENGTH]> = Vec::new();
+        for mac in mac_chunks {
+            macs.push(
+                mac.try_into()
+                    .expect("chunks should be of the required length"),
+            )
+        }
+
+        Ok((bit_value, macs))
+    }
 }
 
 /// The key to authenticate a two-party authenticated bit.
